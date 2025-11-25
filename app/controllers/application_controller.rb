c6b90853905ebf09ed8c0bf644c744ea24593c95
FranckAbeille <franckabeille33@gmail.com>
2025-11-25 15:42:51 +0100

    update applicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:pseudo])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:pseudo])
+  end
+
 end
